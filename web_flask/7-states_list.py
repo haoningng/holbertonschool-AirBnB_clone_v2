d39c963cd1d85a186ffcf37ab05db4849d65bcbe
haoningng <nick.nghn@gmail.com>
2024-04-10 11:41:57 +1000

    fix: move app.teardown_appcontext to bottom

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,16 +9,16 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db():
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def state_list():
     states_list = storage.all(State)
     sorted_list = sorted(states_list.values(), key=lambda s: s.name)
     return render_template('7-states_list.html', states_list=sorted_list)
+
+
+@app.teardown_appcontext
+def teardown_db():
+    storage.close()
 
 
 if __name__ == '__main__':
