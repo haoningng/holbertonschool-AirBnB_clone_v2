6ea0b021c28b223e61316e991d81f7f0b3e709c3
haoningng <nick.nghn@gmail.com>
2024-04-11 18:54:15 +1000

    fix: turn off debug mode, change html file for state_list

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 def state_list():
     states_list = storage.all(State)
     sorted_list = sorted(states_list.values(), key=lambda s: s.name)
-    return render_template('8-cities_by_states.html', states_list=sorted_list)
+    return render_template('7-states_list.html', states_list=sorted_list)
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_state_list():
@@ -27,4 +27,4 @@ def teardown_db(exception=None):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=False)
