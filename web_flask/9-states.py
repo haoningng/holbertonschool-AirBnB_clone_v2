c2911e6c062a245aff2bbddf0911e34bd77396b4
haoningng <nick.nghn@gmail.com>
2024-04-12 18:55:52 +1000

    refactor: combined both states and states/id routes

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,24 +8,18 @@ from models.state import State
 
 app = Flask(__name__)
 
-
 @app.route('/states', strict_slashes=False)
-def states():
-    states_list = storage.all(State)
-    sorted_list = sorted(states_list.values(), key=lambda s: s.name)
-    return render_template('9-states.html', states_list=sorted_list)
-
-
 @app.route('/states/<id>', strict_slashes=False)
-def states_id(id):
+def states_id(id=None):
     states_list = storage.all(State)
     cities_list = []
     state_picked = []
-    for state in states_list.values():
-        if state.id == id:
-            state_picked.append(state)
-            cities_list = sorted(state.cities, key=lambda s: s.name)
-            break
+    if id:
+        for state in states_list.values():
+            if state.id == id:
+                state_picked.append(state)
+                cities_list = sorted(state.cities, key=lambda s: s.name)
+                break
     sorted_list = sorted(states_list.values(), key=lambda s: s.name)
     return render_template('9-states.html',
                            state_picked=state_picked,
