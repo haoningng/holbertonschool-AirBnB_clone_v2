738af9b8deefa2867b1614088335683732500faf
haoningng <nick.nghn@gmail.com>
2024-04-12 11:46:08 +1000

    style: add blank lines to pass pycostyle

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,11 +15,13 @@ def state_list():
     sorted_list = sorted(states_list.values(), key=lambda s: s.name)
     return render_template('7-states_list.html', states_list=sorted_list)
 
+
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_state_list():
     states_list = storage.all(State)
     sorted_list = sorted(states_list.values(), key=lambda s: s.name)
     return render_template('8-cities_by_states.html', states_list=sorted_list)
+
 
 @app.teardown_appcontext
 def teardown_db(exception=None):
